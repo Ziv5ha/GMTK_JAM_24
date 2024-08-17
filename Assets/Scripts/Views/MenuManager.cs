8a8f1a3356440fa55dca7975b8947c07e24d7d63
Ziv <zivshaul3@gmail.com>
2024-08-17 21:54:24 +0300

    Added shop view

--- Assets/Scripts/Views/MenuManager.cs
+++ Assets/Scripts/Views/MenuManager.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class MenuManager: MonoBehaviour {
     //[SerializeField] private MainMenuView MainMenuRef;
-    //[SerializeField] private ShopView ShopViewRef;
+    [SerializeField] private ShopView ShopViewRef;
 
     [SerializeField] private GameObject MainMenuRef;
-    [SerializeField] private GameObject ShopViewRef;
+    //[SerializeField] private GameObject ShopViewRef;
 
     public enum Menus { None, MainMenu, Shop };
 
     private Menus ActiveMenu {
         set {
             //MainMenuRef.gameObject.SetActive(value == Menus.MainMenu);
-            //ShopViewRef.gameObject.SetActive(value == Menus.Shop);
+            ShopViewRef.gameObject.SetActive(value == Menus.Shop);
             MainMenuRef.SetActive(value == Menus.MainMenu);
-            ShopViewRef.SetActive(value == Menus.Shop);
+            //ShopViewRef.SetActive(value == Menus.Shop);
         }
+    }
+
+    public void Init() {
+        ShopViewRef.ECloseShop += CloseAll;
+        GoToMainMenu();
     }
 
     public void CloseAll() {
--- Assets/Scripts/Views/ShopView.cs
+++ Assets/Scripts/Views/ShopView.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class ShopView : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class ShopView: MonoBehaviour {
+    public Action ECloseShop;
+    [SerializeField] private Animator AnimatorRef;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void CloseShop() {
+        AnimatorRef.SetTrigger("CloseShop");
+        Invoke("CloseShopCorotine", 1);
     }
+    private void CloseShopCorotine() {
+        ECloseShop();
+    }
+    //private IEnumerator CloseShopCorotine() {
+    //    CancelInvoke("CloseShopCorotine");
+    //    ECloseShop();
+    //    yield return null;
+    //}
 }
